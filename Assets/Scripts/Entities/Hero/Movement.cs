f9d28f00d25a7a77b3903ff730df0a8517defd45
Nikki Alistra <nikkialistra@gmail.com>
2021-08-17 01:19:38 +0500

    Make base movement and jumps

--- Assets/Scripts/Entities/Hero/Movement.cs
+++ Assets/Scripts/Entities/Hero/Movement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,13 +10,25 @@ namespace Entities.Hero
     public class Movement : MonoBehaviour
     {
         [SerializeField] private float _speed;
-        [SerializeField] private float _jumpForce;
+        [SerializeField] private float _jumpVelocity;
+        [Space]
+        [SerializeField] private float _minGroundNormalY = .65f;
+        [SerializeField] private float _gravityModifier = 1f;
 
-        private Animations _animations;
+        private float _horizontalMove;
+        private Vector2 _velocity;
+        
+        private bool _grounded;
+        private Vector2 _groundNormal;
         
         private Rigidbody2D _rigidBody;
+        
+        private RaycastHit2D[] _hitBuffer = new RaycastHit2D[16];
 
-        private float _moveDirection;
+        private const float _minMoveDistance = 0.001f;
+        private const float _shellRadius = 0.01f;
+
+        private Animations _animations;
 
         private PlayerInput _input;
         private InputAction _moveAction;
@@ -43,35 +56,98 @@ namespace Entities.Hero
 
         private void Update()
         {
-            _moveDirection = _moveAction.ReadValue<float>();
+            _horizontalMove = _moveAction.ReadValue<float>() * _speed;
+            UpdateAnimation();
+        }
+
+        private void UpdateAnimation()
+        {
+            if (_horizontalMove != 0)
+            {
+                _animations.Run(_horizontalMove);
+            }
+            else
+            {
+                _animations.Stay();
+            }
         }
 
         private void FixedUpdate()
         {
-            if (_moveDirection != 0)
-            {
-                Move();
-            }
-            else
-            {
-                Stay();
-            }
+            ComputeVelocity();
+            Move();
+        }
+
+        private void ComputeVelocity()
+        {
+            _velocity += Physics2D.gravity * (_gravityModifier * Time.deltaTime);
+            _velocity.x = _horizontalMove;
         }
 
         private void Move()
         {
-            _animations.Run(_moveDirection);
-            _rigidBody.MovePosition(_rigidBody.position + new Vector2(_moveDirection * _speed * Time.fixedDeltaTime, 0));
+            _grounded = false;
+            
+            var deltaPosition = _velocity * Time.deltaTime;
+            var moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
+
+            var horizontalMove = moveAlongGround * deltaPosition.x;
+            MoveByAxis(horizontalMove, vertical: false);
+
+            var verticalMove = Vector2.up * deltaPosition.y;
+            MoveByAxis(verticalMove, vertical: true);
         }
 
-        private void Stay()
+        private void MoveByAxis(Vector2 move, bool vertical)
         {
-            _animations.Stay();
+            var distance = move.magnitude;
+
+            if (distance < _minMoveDistance)
+            {
+                return;
+            }
+            
+            distance = CalculateCollisions(distance, move, vertical);
+
+            _rigidBody.position = _rigidBody.position + move.normalized * distance;
+        }
+
+        private float CalculateCollisions(float distance, Vector2 move, bool vertical)
+        {
+            var count = _rigidBody.Cast(move, _hitBuffer, distance + _shellRadius);
+
+            for (var i = 0; i < count; i++)
+            {
+                var currentNormal = _hitBuffer[i].normal;
+                if (currentNormal.y > _minGroundNormalY)
+                {
+                    _grounded = true;
+                    if (vertical)
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                var projection = Vector2.Dot(_velocity, currentNormal);
+                if (projection < 0)
+                {
+                    _velocity = _velocity - projection * currentNormal;
+                }
+
+                var modifiedDistance = _hitBuffer[i].distance - _shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+            return distance;
         }
 
         private void Jump(InputAction.CallbackContext context)
         {
-            _rigidBody.AddForce(new Vector2(0, _jumpForce));
+            if (_grounded)
+            {
+                _velocity.y = _jumpVelocity;
+            }
         }
     }
 }
