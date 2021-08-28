c9632c55d577d343c6689b239e692778bb7c4d43
Nikki Alistra <nikkialistra@gmail.com>
2021-08-28 22:32:20 +0500

    Fix cursors glitch

---
+++ Assets/Scripts/Core/CopyTransform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class CopyTransform : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            transform.position = _target.position;
+        }
+    }
+}
